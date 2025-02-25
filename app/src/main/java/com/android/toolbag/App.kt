0efb5ab9cabbb334ec30b28a5c68e90118fa1b51
dear_hj <dear_hj@163.com>
2025-02-25 11:34:52 +0800

    指南针中，P2机型通过方向传感器实现

--- app/src/main/java/com/android/toolbag/App.kt
+++ app/src/main/java/com/android/toolbag/App.kt
@@ -6,6 +6,7 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.hardware.camera2.CameraManager
 import android.preference.PreferenceManager
+import java.io.File
 
 class App : Application() {
     companion object {
@@ -13,6 +14,8 @@ class App : Application() {
         @SuppressLint("StaticFieldLeak")
         var mContext: Context? = null
         var cameraManager: CameraManager? = null
+        private const val P2PRO_PATH = "/sys/devices/platform/gftk_camplight/camplight_mode"
+        val isP2Pro = File(P2PRO_PATH).exists()
     }
 
     override fun onCreate() {
--- app/src/main/java/com/android/toolbag/item/CompassActivity.kt
+++ app/src/main/java/com/android/toolbag/item/CompassActivity.kt
@@ -19,6 +19,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
+import com.android.toolbag.App.Companion.isP2Pro
 import com.android.toolbag.R
 import com.android.toolbag.widget.CompassView
 import java.util.Locale
@@ -28,6 +29,7 @@ class CompassActivity : AppCompatActivity(), SensorEventListener {
     private var mAccelerometerSensor: Sensor? = null
     private var isChinese = false
     private var mMagneticSensor: Sensor? = null
+    private var rotationVectorSensor: Sensor? = null
     private var mPointer: CompassView? = null
     private var mSensorManager: SensorManager? = null
     private val mGravity = FloatArray(3)
@@ -76,71 +78,95 @@ class CompassActivity : AppCompatActivity(), SensorEventListener {
     private fun initServices() {
         mSensorManager = getSystemService("sensor") as SensorManager
         vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-        mMagneticSensor = mSensorManager?.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) //磁场传感器
-        mAccelerometerSensor = mSensorManager?.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) //加速度传感器
-        mSensorManager?.registerListener(
-            this,
-            mAccelerometerSensor,
-            SensorManager.SENSOR_DELAY_FASTEST
-        )
-        mSensorManager?.registerListener(
-            this,
-            mMagneticSensor,
-            SensorManager.SENSOR_DELAY_FASTEST
-        )
+        if (isP2Pro) {  //旋转矢量传感器
+            rotationVectorSensor = mSensorManager?.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)
+            mSensorManager?.registerListener(
+                this,
+                rotationVectorSensor,
+                SensorManager.SENSOR_DELAY_FASTEST
+            )
+        } else {
+            mMagneticSensor = mSensorManager?.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) //磁场传感器
+            mAccelerometerSensor =
+                mSensorManager?.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) //加速度传感器
+            mSensorManager?.registerListener(
+                this,
+                mAccelerometerSensor,
+                SensorManager.SENSOR_DELAY_FASTEST
+            )
+            mSensorManager?.registerListener(
+                this,
+                mMagneticSensor,
+                SensorManager.SENSOR_DELAY_FASTEST
+            )
+        }
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
         val alpha = 0.98f //平滑因子
 
         synchronized(this) {
-            //指南针转动角度算法
-            //判断当前是加速度感应器还是地磁感应器
-            if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
-                mGravity[0] = (alpha * mGravity[0] + (1 - alpha) * event.values[0])
-                mGravity[1] = (alpha * mGravity[1] + (1 - alpha) * event.values[1])
-                mGravity[2] = (alpha * mGravity[2] + (1 - alpha) * event.values[2])
-            }
+            if (!isP2Pro) {
+                //指南针转动角度算法
+                //判断当前是加速度感应器还是地磁感应器
+                if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
+                    mGravity[0] = (alpha * mGravity[0] + (1 - alpha) * event.values[0])
+                    mGravity[1] = (alpha * mGravity[1] + (1 - alpha) * event.values[1])
+                    mGravity[2] = (alpha * mGravity[2] + (1 - alpha) * event.values[2])
+                }
 
-            if (event?.sensor?.type == Sensor.TYPE_MAGNETIC_FIELD) {
-                mGeomagnetic[0] = (alpha * mGeomagnetic[0] + (1 - alpha) * event.values[0])
-                mGeomagnetic[1] = (alpha * mGeomagnetic[1] + (1 - alpha) * event.values[1])
-                mGeomagnetic[2] = (alpha * mGeomagnetic[2] + (1 - alpha) * event.values[2])
-            }
+                if (event?.sensor?.type == Sensor.TYPE_MAGNETIC_FIELD) {
+                    mGeomagnetic[0] = (alpha * mGeomagnetic[0] + (1 - alpha) * event.values[0])
+                    mGeomagnetic[1] = (alpha * mGeomagnetic[1] + (1 - alpha) * event.values[1])
+                    mGeomagnetic[2] = (alpha * mGeomagnetic[2] + (1 - alpha) * event.values[2])
+                }
 
-            val rArray = FloatArray(9)
-            val iArray = FloatArray(9)
-            val success = SensorManager.getRotationMatrix(rArray, iArray, mGravity, mGeomagnetic)
-            if (success) {
-                val orientation = FloatArray(3)
-                SensorManager.getOrientation(rArray, orientation)
-                azimuth = Math.toDegrees(orientation[0].toDouble()).toFloat()
-                azimuth = (azimuth + 360) % 360
-                adjustArrow()
+                val rArray = FloatArray(9)
+                val iArray = FloatArray(9)
+                val success =
+                    SensorManager.getRotationMatrix(rArray, iArray, mGravity, mGeomagnetic)
+                if (success) {
+                    val orientation = FloatArray(3)
+                    SensorManager.getOrientation(rArray, orientation)
+                    azimuth = Math.toDegrees(orientation[0].toDouble()).toFloat()
+                    azimuth = (azimuth + 360) % 360
+                    adjustArrow()
+                }
+            } else {
+                if (event?.sensor?.type == Sensor.TYPE_ROTATION_VECTOR) {
+                    val rotationMatrix = FloatArray(9)
+                    SensorManager.getRotationMatrixFromVector(rotationMatrix, event.values)
+                    val orientation = FloatArray(3)
+                    SensorManager.getOrientation(rotationMatrix, orientation)
+                    azimuth = Math.toDegrees(orientation[0].toDouble()).toFloat() // 方位角
+                    adjustArrow()
+                }
             }
         }
     }
 
 
     override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-        if (p0?.type == Sensor.TYPE_MAGNETIC_FIELD) {
-            if (p1 == SensorManager.SENSOR_STATUS_ACCURACY_HIGH || p1 == SensorManager.SENSOR_STATUS_ACCURACY_MEDIUM) {
-                if (calibrationView?.visibility == View.VISIBLE) {
-                    Toast.makeText(
-                        this,
-                        getString(R.string.calibration_success),
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    //震动提示
-                    if (vibrator != null && vibrator!!.hasVibrator()) {
-                        vibrator!!.vibrate(500L)
+        if (!isP2Pro) {
+            if (p0?.type == Sensor.TYPE_MAGNETIC_FIELD) {
+                if (p1 == SensorManager.SENSOR_STATUS_ACCURACY_HIGH || p1 == SensorManager.SENSOR_STATUS_ACCURACY_MEDIUM) {
+                    if (calibrationView?.visibility == View.VISIBLE) {
+                        Toast.makeText(
+                            this,
+                            getString(R.string.calibration_success),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        //震动提示
+                        if (vibrator != null && vibrator!!.hasVibrator()) {
+                            vibrator!!.vibrate(500L)
+                        }
                     }
+                    refreshLayout(false)
+                    println("指南针无须校准")
+                } else {
+                    refreshLayout(true)
+                    println("指南针需要校准")
                 }
-                refreshLayout(false)
-                println("指南针无须校准")
-            } else {
-                refreshLayout(true)
-                println("指南针需要校准")
             }
         }
     }
